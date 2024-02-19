a258367878b84e5a25be59d0bcaadebcadc3f324
serap <serap@DESKTOP-JIBKOBL>
2024-02-19 21:56:53 +0300

    Add project files.

---
+++ Business/Concrete/CargoManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CargoManager : ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public CargoManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+        
+        public void GiveMaterial(Person person) 
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine("Material were given for " + person.FirstName);
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personOne = new Person();
+
+            personOne.NationalIdentity = 12345678910;
+            personOne.FirstName = "Test";
+            personOne.LastName = "Test";
+            personOne.YearOfBirthday = 1234;
+
+
+            CargoManager cargoManager = new CargoManager(new NativerManager());
+            cargoManager.GiveMaterial(personOne);
+
+
+        }
+    }
+}
+
